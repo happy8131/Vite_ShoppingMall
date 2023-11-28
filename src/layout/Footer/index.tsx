a0b9fc35e674e771159da81ec4584708f92615d2
happy8131 <lion8131@naver.com>
2023-11-28 22:25:36 +0900

    space

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -2,7 +2,7 @@ import { AiOutlineSmile } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div className="flex h-20 text-lg justify-center items-center  ">
+    <div className="flex h-20 text-lg justify-center items-center">
       All rights reserved. <AiOutlineSmile />
     </div>
   );
